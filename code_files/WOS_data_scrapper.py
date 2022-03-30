47f3e843a2abb1ad30decdd1346971f07dd09066
Pavan Balaji Kumar <pavanbalaji22@gmail.com>
2022-03-29 20:40:41 -0400

    Download part complete

--- code_files/WOS_data_scrapper.py
+++ code_files/WOS_data_scrapper.py
@@ -11,10 +11,12 @@ import time
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-
+### Initialization Part###
 driver = webdriver.Chrome(PATH)
 driver.get("https://login.proxy.bib.uottawa.ca/login?url=https://www.webofknowledge.com/wos")
 driver.execute_script("shibAuth()")
+
+### Login Part###
 driver.find_element_by_id("i0116").send_keys("pkumar2@uottawa.ca")
 driver.find_element_by_id("idSIButton9").click()
 driver.find_element_by_id("i0118").send_keys("Astayuno1129!")
@@ -28,6 +30,7 @@ time.sleep(1)
 driver.find_element_by_id("pendo-close-guide-ecbac349").click()
 time.sleep(1)
 
+### Search Part ###
 driver.find_element_by_link_text("RESEARCHERS").click()
 time.sleep(3)
 driver.find_element_by_id("mat-input-0").send_keys("Elias") # Last name field
@@ -37,13 +40,13 @@ driver.find_element_by_xpath("//button[@cdxanalyticscategory = 'WOS-authorsearch
 time.sleep(3)
 driver.find_element_by_partial_link_text("Elias").click()
 time.sleep(1)
+
+### Download Part ###
 driver.find_element_by_link_text('View citation report').click()
 time.sleep(2)
-# driver.find_element_by_link_text('Export Full Report').click()
 driver.find_element_by_xpath("//button[@aria-label = 'Export Full Report']").click()
 time.sleep(2)
 driver.find_element_by_id("mat-input-2").send_keys("Ashley Elias")
-# driver.find_element_by_xpath("//input[@value = 'crx']").send_keys(Keys.ENTER)
 time.sleep(2)
 driver.find_element_by_id("mat-radio-5").click()
 driver.find_element_by_xpath("//button[@class = 'standard-button primary-button']").click()
